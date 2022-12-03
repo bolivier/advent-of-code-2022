1b7b3f5ccbd234614a1b7bdbe73f5938bbcc7da7
Brandon Olivier <brandon@brandonolivier.com>
2022-12-03 17:36:43 -0600

    Solve day3 part 2

--- src/advent_of_code/2022/day_3.clj
+++ src/advent_of_code/2022/day_3.clj
@@ -10,15 +10,15 @@ wMqvLMZHhHMvwLHjbvcjnnSBnvTQFn
 ttgJtRGJQctTZtZT
 CrZsJsPPZsGzwwsLwLmpwMDw")
 
-(defn halve [coll]
-  (let [size (count coll)]
+(defn halve [string]
+  (let [coll (str/split string #"")
+        size (count coll)]
     [(into #{} (subvec coll 0 (int (/ size 2))))
      (into #{} (subvec coll (int (/ size 2))))]))
 
 (defn parse [file-contents]
-  (map
-   (fn [line]
-     (halve (str/split line #"")))
+  (map (fn [rucksack]
+         (halve rucksack))
    (str/split-lines file-contents)))
 
 (def get-priority
@@ -76,17 +76,35 @@ CrZsJsPPZsGzwwsLwLmpwMDw")
    "Y" 51
    "Z" 52})
 
+(defn all-rucksack-contents [rucksack]
+  (let [[pocket-1 pocket-2] rucksack]
+   (set/union pocket-1 pocket-2)))
+
 (defn get-file-contents []
   (slurp "inputs/2022/day-3"))
 
 (defn solution []
   (let [file-contents (get-file-contents)
-        input (parse file-contents)
+        rucksacks (parse file-contents)
         overlapping-items (map (fn [[rucksack1 rucksack2]]
+                                 ;; there should only be 1 element
                                  (first (set/intersection rucksack1 rucksack2)))
-                               input)]
+                               rucksacks)]
     (apply + (map get-priority overlapping-items))))
 
 (comment
   (solution);; => 8109
+)
+
+(defn solution2 []
+  (let [file-contents (get-file-contents)
+        rucksacks (map all-rucksack-contents (parse file-contents))
+        rucksack-groups (partition 3 rucksacks)
+        badge-item-types (map (fn [group]
+                                (first (apply set/intersection group)))
+                              rucksack-groups)]
+    (apply + (map get-priority badge-item-types))))
+
+(comment
+  (solution2);; => 2738
 )
