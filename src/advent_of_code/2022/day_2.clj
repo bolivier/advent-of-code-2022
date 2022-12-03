c54d76034ba849fd737ae11714fab4207a2c069e
Brandon Olivier <brandon@brandonolivier.com>
2022-12-03 16:32:22 -0600

    Finish day 2

--- src/advent_of_code/2022/day_2.clj
+++ src/advent_of_code/2022/day_2.clj
@@ -14,9 +14,10 @@
 B X
 C Z")
 
-(def defeated-by {:rock     :scissors
-                  :paper    :rock
-                  :scissors :paper})
+(def defeated-by {:rock :scissors
+                  :paper     :rock
+                  :scissors    :paper})
+(def defeats (clojure.set/map-invert defeated-by))
 
 (defn win? [them me]
   (= them (defeated-by me)))
@@ -31,47 +32,66 @@ C Z")
                   :paper    2
                   :scissors 3})
 
-(defmulti score-round
-  (fn [them me]
-    (cond
-      (win? them me)
-      :win
-
-      (tie? them me)
-      :tie
-
-      :else
-      :loss)))
-
-(defmethod score-round :win [them me]
-  (+ (shape-score me) win-bonus))
-
-(defmethod score-round :tie [them me]
-  (+ (shape-score me) tie-bonus))
-
-(defmethod score-round :loss [them me]
-  (shape-score me))
-
-
-
 (defprotocol Scoreable
   (score [round] "Score a round of rock, paper, scissors"))
 
 (defrecord ShapeRound [their-shape my-shape]
   Scoreable
   (score [{:keys [their-shape my-shape]}]
-    (score-round their-shape my-shape)))
+    [their-shape my-shape]
+  (cond
+    (win? their-shape my-shape)
+    (+ (shape-score my-shape) win-bonus)
 
-(defrecord WinLossRound [their-shape outcome])
+    (tie? their-shape my-shape)
+    (+ (shape-score my-shape) tie-bonus)
 
+    :else
+    (shape-score my-shape))))
+
+(def lookup-outcome {"X" :loss
+                     "Y" :tie
+                     "Z" :win})
+
+(defrecord WinLossRound [their-shape outcome]
+  Scoreable
+  (score [{:keys [their-shape outcome]}]
+    (case outcome
+      :win
+      (score (->ShapeRound their-shape (defeats their-shape)))
+      :loss
+      (score (->ShapeRound their-shape (defeated-by their-shape)))
+      :tie
+      (score (->ShapeRound their-shape their-shape)))))
+
+(defn- parse-file-contents [file-contents]
+  (map #(str/split % #" ")
+       (str/split-lines file-contents)))
+
+(defn read-input []
+  (let [file-contents (slurp "inputs/2022/day-2")]
+    (parse-file-contents file-contents)))
+
+(comment (def input (read-input)))
 
 (defn solution []
-  (let [file-contents (slurp "inputs/2022/day-2")]
-   (reduce + (map
-              #(apply score-round %)
-
-              (map #(map lookup %) (map #(str/split % #" ")
-                                        (str/split-lines file-contents)))))))
+  (let [input (read-input)
+        create-shape-round (fn [[them me]]
+                             (->ShapeRound (lookup them) (lookup me)))]
+    (->> input
+         (map create-shape-round)
+         (map score)
+         (reduce +))))
 
 (comment (solution );; => 12679
 )
+
+(defn solution2 []
+  (let [input (read-input)
+        create-win-loss-round
+        (fn [[them outcome]]
+          (->WinLossRound (lookup them) (lookup-outcome outcome)))]
+    (->> input
+         (map create-win-loss-round)
+         (map score)
+         (reduce +))))
